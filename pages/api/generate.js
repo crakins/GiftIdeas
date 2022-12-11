68109591acc5511267bfa8912a4eb4cbbff7d333
crakins <crakins@ChristohersiMac.tx.rr.com>
2022-12-11 12:23:17 -0600

    initial commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,7 +7,7 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt: generatePrompt(req.body.animal),
     temperature: 0.6,
   });
@@ -17,12 +17,12 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+  return `Suggest three names for an animal that is fun for kids.
 
 Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Names: Fluffy, Bella, Lucy
 Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Names: Odie, Max, cooper
 Animal: ${capitalizedAnimal}
 Names:`;
 }

ce71efcddc462a553176dc08887acfa468b1e37a
crakins <4482580+crakins@users.noreply.github.com>
2023-04-20 00:08:04 -0500

    fixed date

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -2,10 +2,13 @@ import React from "react";
 import styles from './footer.module.css';
 
 function Footer() {
+
+    const year = new Date();
+
     return (
         <main className={styles.main}>
             <div className={styles.hr}></div>
-            &copy; 2022 <a href="https://crakins.com">crakins.com</a>
+            &copy; {year.getFullYear()} <a href="https://crakins.com">crakins.com</a>
         </main>
     );
 };
